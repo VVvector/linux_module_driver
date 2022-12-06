8753c0fbcec3a1c8c62d9cc45a7d6185af58f147
shuzhaoping <1147059951@qq.com>
2022-12-06 21:21:56 +0800

    enhance tc python

--- tc_tethering/tc_tethering_bpf_coordinator_v2.py
+++ tc_tethering/tc_tethering_bpf_coordinator_v2.py
@@ -9,11 +9,11 @@ import time
 
 
 def run_cmd(cmd_string, timeout=20):
-    print("cmd：" + cmd_string)
+    log.info("cmd：{}".format(cmd_string))
     p = subprocess.Popen(cmd_string, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True, close_fds=True,
                          start_new_session=True)
 
-    format = 'utf-8'
+    txt_format = 'utf-8'
     if platform.system() == "Windows":
         format = 'gbk'
 
@@ -22,10 +22,10 @@ def run_cmd(cmd_string, timeout=20):
         ret_code = p.poll()
         if ret_code:
             code = 1
-            msg = "[Error]Called Error ： " + str(msg.decode(format))
+            msg = "[Error]Called Error ： " + str(msg.decode(txt_format))
         else:
             code = 0
-            msg = str(msg.decode(format))
+            msg = str(msg.decode(txt_format))
     except subprocess.TimeoutExpired:
         # 注意：不能只使用p.kill和p.terminate，无法杀干净所有的子进程，需要使用os.killpg
         p.kill()
@@ -34,18 +34,24 @@ def run_cmd(cmd_string, timeout=20):
 
         # 注意：如果开启下面这两行的话，会等到执行完成才报超时错误，但是可以输出执行结果
         # (outs, errs) = p.communicate()
-        # print(outs.decode('utf-8'))
+        # log.info(outs.decode('utf-8'))
 
         code = 1
-        msg = "[ERROR]Timeout Error : Command '" + cmd_string + "' timed out after " + str(timeout) + " seconds"
+        msg = "Timeout Error : Command '" + cmd_string + "' timed out after " + str(timeout) + " seconds"
     except Exception as e:
         code = 1
-        msg = "[ERROR]Unknown Error : " + str(e)
+        msg = "Unknown Error : " + str(e)
 
     return code, msg
 
 
-class CLIENT:
+class Client:
+    def __init__(self):
+        self.l4_port = None
+        self.l4_proto = None
+        self.ipv4_addr = None
+        self.mac_addr = None
+
     def update_info(self, mac_addr, ipv4_addr, l4_proto, l4_port):
         self.mac_addr = mac_addr
         self.ipv4_addr = ipv4_addr
@@ -54,7 +60,12 @@ class CLIENT:
         log.info("client: {}-{}-{}-{}".format(self.mac_addr, self.ipv4_addr, self.l4_proto, self.l4_port))
 
 
-class SERVER:
+class Server:
+    def __init__(self):
+        self.l4_port = None
+        self.l4_proto = None
+        self.ipv4_addr = None
+
     def update_info(self, ipv4_addr, l4_proto, l4_port):
         self.ipv4_addr = ipv4_addr
         self.l4_proto = l4_proto
@@ -103,10 +114,11 @@ def parse_nic_information(nic_name):
     return if_id, mac_addr, ipv4_addr
 
 
-class DOWNSTREAM:
+class Downstream:
     l3_proto = "0x0800"  # ipv4
 
     def __init__(self, nic_name):
+        self.l4_port = None
         self.name = nic_name
         if_id, mac_addr, ipv4_addr = parse_nic_information(self.name)
         self.if_id = if_id
@@ -120,8 +132,9 @@ class DOWNSTREAM:
                                                    self.l4_port))
 
 
-class UPSTREAM:
+class Upstream:
     def __init__(self, nic_name):
+        self.l4_port = None
         self.name = nic_name
         if_id, mac_addr, ipv4_addr = parse_nic_information(self.name)
         self.if_id = if_id
@@ -138,25 +151,7 @@ class UPSTREAM:
             return False
 
 
-###################neighbor ######################################################################################
-def set_nat_rule(client_ipv4_addr, upstream: UPSTREAM):
-    cmd = "iptables -t nat -A POSTROUTING -o {} -s {}  -j SNAT --to {}".format(
-        upstream.name, client_ipv4_addr, upstream.ipv4_addr)
-    ret, msg = run_cmd(cmd)
-    log.debug(msg)
-
-    cmd = "iptables -t nat -vnL"
-    ret, msg = run_cmd(cmd)
-    log.info("nat rule: {}".format(msg))
-
-    cmd = "echo 1 > /proc/sys/net/ipv4/ip_forward"
-    run_cmd(cmd)
-    cmd = "cat /proc/sys/net/ipv4/ip_forward"
-    ret, msg = run_cmd(cmd)
-    log.debug("ipv4 forward status: {}".format(msg))
-
-
-class NEIGHBOR_MESSAGE:
+class NeighborMessage:
 
     def __init__(self, neighbor_msg):
         self.msg = neighbor_msg
@@ -166,7 +161,7 @@ class NEIGHBOR_MESSAGE:
     def parse_neighbor_msg(self):
         temp = self.msg.split()
         log.debug(temp)
-        if (len(temp) != 4):
+        if len(temp) != 4:
             log.info("not complete neighbor message: {}".format(self.msg))
             return -1
         self.ipv4_addr = temp[0]
@@ -175,13 +170,20 @@ class NEIGHBOR_MESSAGE:
         return 0
 
 
-class NEIGHBOR_MONITOR:
+def check_neighbor_equal(src: NeighborMessage, dst: NeighborMessage):
+    if src.ipv4_addr == dst.ipv4_addr:
+        return True
+    else:
+        return False
 
-    def __init__(self, dev_name, link_info, callback):
+
+class NeighborMonitor:
+
+    def __init__(self, dev_name, link_information, callback):
         self.neighbor_message_list = []
         self.neighbor_msg = None
         self.dev_name = dev_name
-        self.link_info = link_info
+        self.link_info = link_information
         self.callback = callback
 
     def dump_neighbor_msgs(self):
@@ -191,16 +193,10 @@ class NEIGHBOR_MONITOR:
         self.neighbor_msg = msg
         return msg
 
-    def check_neighbor_equle(self, src: NEIGHBOR_MESSAGE, dst: NEIGHBOR_MESSAGE):
-        if (src.ipv4_addr == dst.ipv4_addr):
-            return True
-        else:
-            return False
-
-    def check_neighbor_msg_is_exist(self, neighbor_msg: NEIGHBOR_MESSAGE):
+    def check_neighbor_msg_is_exist(self, neighbor_msg: NeighborMessage):
         is_exist = False
         for item in self.neighbor_message_list:
-            if self.check_neighbor_equle(neighbor_msg, item):
+            if check_neighbor_equal(neighbor_msg, item):
                 is_exist = True
                 break
 
@@ -230,7 +226,7 @@ class NEIGHBOR_MONITOR:
         msgs = self.neighbor_msg.splitlines()
         log.debug(msgs)
         for msg in msgs:
-            neighbor_message = NEIGHBOR_MESSAGE(msg)
+            neighbor_message = NeighborMessage(msg)
             if neighbor_message.parse_neighbor_msg() == -1:
                 continue
 
@@ -243,16 +239,16 @@ class NEIGHBOR_MONITOR:
             self.callback(neighbor_message.ipv4_addr, self.link_info["upstream"])
 
 
-######### conntrack monitor ########################
-class TUPLE:
-    proto_num = None
-    src_ip = None
-    dst_ip = None
-    src_port = None
-    dst_port = None
+class ConnectionTuple:
+    def __init__(self):
+        self.proto_num = None
+        self.src_ip = None
+        self.dst_ip = None
+        self.src_port = None
+        self.dst_port = None
 
 
-def check_tuple_equle(src: TUPLE, dst: TUPLE):
+def check_tuple_equal(src: ConnectionTuple, dst: ConnectionTuple):
     if (src.proto_num == dst.proto_num and src.src_ip == dst.src_ip and src.dst_ip == dst.dst_ip and
             src.src_port == dst.src_port and src.dst_port == dst.dst_port):
         return True
@@ -260,22 +256,23 @@ def check_tuple_equle(src: TUPLE, dst: TUPLE):
         return False
 
 
-def check_tuple_is_valid(tuple: TUPLE):
-    if (tuple.proto_num is not None and tuple.src_ip is not None and tuple.dst_ip is not None and
-            tuple.src_port is not None and tuple.dst_port is not None):
+def check_tuple_is_valid(connection_tuple: ConnectionTuple):
+    if (connection_tuple.proto_num is not None and connection_tuple.src_ip is not None and
+            connection_tuple.dst_ip is not None and connection_tuple.src_port is not None and
+            connection_tuple.dst_port is not None):
         return True
     else:
         return False
 
 
-class CONNTRACK_MESSAGE:
+class ConntrackMessage:
 
     def __init__(self, conntrack_msg):
         self.msg = conntrack_msg
         # Original direction conntrack tuple.
-        self.tuple_orig = TUPLE()
+        self.tuple_orig = ConnectionTuple()
         # Reply direction conntrack tuple.
-        self.tuple_reply = TUPLE()
+        self.tuple_reply = ConnectionTuple()
         self.orig_src_mac = None
 
     def parse_conntrack_msg(self):
@@ -324,15 +321,15 @@ class CONNTRACK_MESSAGE:
                                                       self.tuple_reply.dst_port))
 
 
-class CONNTRACK_MONITOR:
+class ConntrackMonitor:
     conntrack_message_list = []
     conntrack_msg = None
 
-    def __init__(self, monitor_interval, link_info, callback):
+    def __init__(self, monitor_interval, link_information, callback):
         self.callback = callback
         self.monitor_interval = monitor_interval
-        self.neighbor_monitor: NEIGHBOR_MONITOR = link_info["neighbor"]
-        self.upstream: UPSTREAM = link_info["upstream"]
+        self.neighbor_monitor: NeighborMonitor = link_information["neighbor"]
+        self.upstream: Upstream = link_information["upstream"]
 
     def dump_conntrack_msgs(self):
         cmd = "conntrack -L"
@@ -341,20 +338,20 @@ class CONNTRACK_MONITOR:
         self.conntrack_msg = msg
         return msg
 
-    def need_offload_conntrack_event(self, conntrack_msg: CONNTRACK_MESSAGE):
-        if conntrack_msg.msg.find("ESTABLISHED") != -1 and \
-                self.neighbor_monitor.check_neighbor_is_exit_by_ipv4_addr(conntrack_msg.tuple_orig.src_ip) and \
-                self.upstream.check_ip_equal_upstream(conntrack_msg.tuple_reply.dst_ip):
+    def need_offload_conntrack_event(self, conntrack_msg: ConntrackMessage):
+        if (conntrack_msg.msg.find("ESTABLISHED") != -1 and
+                self.neighbor_monitor.check_neighbor_is_exit_by_ipv4_addr(conntrack_msg.tuple_orig.src_ip) and
+                self.upstream.check_ip_equal_upstream(conntrack_msg.tuple_reply.dst_ip)):
             conntrack_msg.orig_src_mac = self.neighbor_monitor.get_neighbor_mac_by_ip(conntrack_msg.tuple_orig.src_ip)
             return True
         else:
             return False
 
-    def check_contrack_msg_is_exist(self, conntrack_msg: CONNTRACK_MESSAGE):
+    def check_contrack_msg_is_exist(self, conntrack_msg: ConntrackMessage):
         is_exist = False
         for item in self.conntrack_message_list:
-            if check_tuple_equle(conntrack_msg.tuple_orig, item.tuple_orig) and \
-                    check_tuple_equle(conntrack_msg.tuple_reply, item.tuple_reply):
+            if (check_tuple_equal(conntrack_msg.tuple_orig, item.tuple_orig) and
+                    check_tuple_equal(conntrack_msg.tuple_reply, item.tuple_reply)):
                 is_exist = True
                 break
 
@@ -365,7 +362,7 @@ class CONNTRACK_MONITOR:
         self.dump_conntrack_msgs()
         msgs = self.conntrack_msg.splitlines()
         for msg in msgs:
-            conntrack_msg = CONNTRACK_MESSAGE(msg)
+            conntrack_msg = ConntrackMessage(msg)
             if conntrack_msg.parse_conntrack_msg() != 0:
                 continue
             if self.check_contrack_msg_is_exist(conntrack_msg) is True:
@@ -386,12 +383,12 @@ class CONNTRACK_MONITOR:
 link_info = dict()
 
 
-def set_tc_tethering_offload_rule(conntrack_msg: CONNTRACK_MESSAGE):
+def set_tc_tethering_offload_rule(conntrack_msg: ConntrackMessage):
     log.info("add tethering offload rule:")
-    downstream: DOWNSTREAM = link_info["downstream"]
-    upstream: UPSTREAM = link_info["upstream"]
-    client: CLIENT = link_info["client"]
-    server: SERVER = link_info["server"]
+    downstream: Downstream = link_info["downstream"]
+    upstream: Upstream = link_info["upstream"]
+    client: Client = link_info["client"]
+    server: Server = link_info["server"]
 
     downstream.update_info(conntrack_msg.tuple_orig.dst_port)
     upstream.update_info(conntrack_msg.tuple_reply.dst_port)
@@ -421,7 +418,7 @@ def set_tc_tethering_offload_rule(conntrack_msg: CONNTRACK_MESSAGE):
     log.info("tc_tethering_user log:  ret={}, msg={}".format(ret, msg))
 
 
-def load_tc_bpf(downstream: DOWNSTREAM, upstream: UPSTREAM):
+def load_tc_bpf(downstream: Downstream, upstream: Upstream):
     # load downstream NIC tc ingress - upstream4_map
     cmd = "tc qdisc show dev {}".format(downstream.name)
     run_cmd(cmd)
@@ -453,7 +450,28 @@ def load_tc_bpf(downstream: DOWNSTREAM, upstream: UPSTREAM):
     log.info("dev {} ingress: {}".format(upstream.name, msg))
 
 
+def set_nat_rule(client_ipv4_addr, upstream: Upstream):
+    cmd = "iptables -t nat -A POSTROUTING -o {} -s {}  -j SNAT --to {}".format(
+        upstream.name, client_ipv4_addr, upstream.ipv4_addr)
+    ret, msg = run_cmd(cmd)
+    log.debug(msg)
+
+    cmd = "iptables -t nat -vnL"
+    ret, msg = run_cmd(cmd)
+    log.info("nat rule: {}".format(msg))
+
+    cmd = "echo 1 > /proc/sys/net/ipv4/ip_forward"
+    run_cmd(cmd)
+    cmd = "cat /proc/sys/net/ipv4/ip_forward"
+    ret, msg = run_cmd(cmd)
+    log.debug("ipv4 forward status: {}".format(msg))
+
+
 def clean_setting(upstream_nic_name, downstream_nic_name):
+    log.info("disable ipv4 forward:")
+    cmd = "echo 1 > /proc/sys/net/ipv4/ip_forward"
+    run_cmd(cmd)
+
     log.info("clean NAT setting:")
     cmd = "iptables -t nat -F"
     run_cmd(cmd)
@@ -482,6 +500,8 @@ def main():
     parser.add_argument('-r', '--recovery', action='store_true', help='clean the iptabels NAT setting')
     args = parser.parse_args()
 
+    log.basicConfig(level=log.INFO, format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
     if args.downstream_nic_name is None or args.upstream_nic_name is None:
         log.info("please input the downstream_nic_name, upstream_nic_name")
         return
@@ -490,10 +510,10 @@ def main():
         clean_setting(args.upstream_nic_name, args.downstream_nic_name)
         return
 
-    downstream = DOWNSTREAM(args.downstream_nic_name)
-    upstream = UPSTREAM(args.upstream_nic_name)
-    client = CLIENT()
-    server = SERVER()
+    downstream = Downstream(args.downstream_nic_name)
+    upstream = Upstream(args.upstream_nic_name)
+    client = Client()
+    server = Server()
 
     load_tc_bpf(downstream, upstream)
 
@@ -502,15 +522,13 @@ def main():
     link_info["client"] = client
     link_info["server"] = server
 
-    neighbor_monitor = NEIGHBOR_MONITOR(args.downstream_nic_name, link_info, set_nat_rule)
+    neighbor_monitor = NeighborMonitor(args.downstream_nic_name, link_info, set_nat_rule)
     neighbor_monitor.update_neighbor_messages()
     link_info["neighbor"] = neighbor_monitor
 
-    conntrack = CONNTRACK_MONITOR(2, link_info, set_tc_tethering_offload_rule)
+    conntrack = ConntrackMonitor(2, link_info, set_tc_tethering_offload_rule)
     conntrack.monitor_conntrack()
 
 
 if __name__ == '__main__':
-    log.basicConfig(level=log.INFO,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     main()
